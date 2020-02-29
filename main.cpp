030e0bffb7814380f17586642a26b97b035a6334
ssp4all <2015suraj.pawar@ves.ac.in>
2020-02-29 12:17:23 -0500

    Done mat mul

--- main.cpp
+++ main.cpp
@@ -1,42 +1,47 @@
-// Suraj Pawar
+// @uthor: Suraj Sunil Pawar
+//Matrix Multiplication and Transpose
+
 #include <iostream>
 #include <vector>
 #include "mat.hpp"
 using namespace std;
-#define nl "\n"   
+#define nl "\n"
 
-int main(){
-    
+int main()
+{
+
     long long p = 2, q = 2, r = 2;
 
-    vector< vector<int> > a;
-    vector< vector<int> > b;
+    vector<vector<int>> a;
+    vector<vector<int>> b;
 
-    for(int i = 0; i < 2; i++) {
+    for (int i = 0; i < 2; i++)
+    {
         vector<int> v;
-        for(int j = 0; j < 2; j++) {
+        for (int j = 0; j < 2; j++)
+        {
             v.push_back(1);
         }
         a.push_back(v);
     }
-    for(int i = 0; i < 2; i++) {
+    for (int i = 0; i < 2; i++)
+    {
         vector<int> v;
-        for(int j = 0; j < 2; j++) {
+        for (int j = 0; j < 2; j++)
+        {
             v.push_back(1);
         }
         b.push_back(v);
     }
-    vector< vector<int> > c(2, vector<int>(2, 0));
-    // try{
-    matrix_mul(a, b, c, p, q, r);
-    // }
-    // catch(error e){
-    //     cout << "Error!" << "\n"; 
-    // }
-    for (int i = 0; i < p; ++i){
+    vector<vector<int>> c(2, vector<int>(2, 0));
+
+    matrix_mul(a, b, c);
+
+    for (int i = 0; i < p; ++i)
+    {
         for (int j = 0; j < r; ++j)
             cout << c[i][j] << "\t";
         cout << nl;
     }
-    return 0;   
+    return 0;
 }
--- mat.hpp
+++ mat.hpp
@@ -4,14 +4,47 @@
 using namespace std;
 
 template <class T>
-void matrix_mul(vector< vector<T> > a, vector< vector<T> > b, vector< vector<T> > &c, long long p, long long q, long long r){
-    try{
-        for (int i = 0; i < p; ++i)
-            for (int j = 0; j < q; ++j)
-                for (int k = 0; k < r; ++k)
+void matrix_mul(vector<vector<T>> a, vector<vector<T>> b, vector<vector<T>> &c)
+{
+    try
+    {
+        long long nr_a = a.size();
+        long long nc_a = a[0].size();
+
+        long long nr_b = b.size();
+        long long nc_b = b[0].size();
+
+        long long nr_c = c.size();
+        long long nc_c = c[0].size();
+
+        if (nc_a != nr_b or nr_c != nr_a or nc_c != nc_b)
+        {
+            throw "";
+        }
+        if (nr_a <= 0 || nc_a <= 0 || nc_b <= 0 || nr_b <= 0 || nr_c <= 0 || nc_c <= 0)
+            throw "";
+        for (long long i = 0; i < nr_a; ++i)
+            for (long long j = 0; j < nc_a; ++j)
+                for (long long k = 0; k < nc_b; ++k)
                     c[i][j] += a[i][k] * b[k][j];
     }
-    catch(...){
-        cout << "Dimension cannot be zero!" << "nl";
+    catch (...)
+    {
+        cout << "Err...Check dimensions...U know what I mean!"
+             << "nl";
+    }
+}
+
+template <class T>
+void matrix_mul(vector<vector<T>> a, vector<vector<T>> &b)
+{
+    try
+    {
+        
+    }
+    catch (...)
+    {
+        cout << "Err...Check dimensions...U know what I mean!"
+             << "nl";
     }
 }
